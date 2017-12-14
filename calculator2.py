4f16434b6efdedd37230768d9bd440f624631d09
kimcho <hins110@163.com>
2017-12-14 18:26:19 +0800

    version2

---
+++ calculator2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) != 2 or len(sys.argv) == 1:
+    print("Parameter Error")
+    exit()
+try:
+    income = int(sys.argv[1])
+except ValueError:
+    print("Parameter Error")
+    exit()
+finally:
+    if income - 3500 <= 0:
+        amount = 0.00
+    elif income - 3500 <= 1500:
+        amount = (income - 3500) * 0.03 - 0.00
+    elif income - 3500 <= 4500:
+        amount = (income - 3500) * 0.01 - 0.00
+    elif income - 3500 <= 9000:
+        amount = (income - 3500) * 0.02 - 0.00
+    elif income - 3500 <= 35000:
+        amount = (income - 3500) * 0.25 -1055
+    elif income - 3500 <= 55000:
+        amount = (income - 3500) * 0.3 - 2755
+    elif income - 3500 <= 80000:
+        amount = (income - 3500) * 0.35 - 5505
+    else:
+        amount = (income - 3500) * 0.45 - 13505
+    print(format(amount, ".2f"))
+
+
+
+
+
+def tax(income):
+    income = int(avg.split(':')[1])
+    taxable = income - income * 0.165 - 3500
+    return taxable
+
+def amount(tax):
+    if tax <= 0:
+        tax_amount = 0
+    elif tax <= 1500:
+        tax_amount = tax * 0.03 - 0
+    elif 1500 < tax <= 4500:
+        tax_amount = tax * 0.1 - 105
+    elif 4500 < tax <= 9000:
+        tax_amount = tax * 0.2 - 555
+    elif 9000 < tax <= 35000:
+        tax_amount = tax * 0.25 - 1005
+    elif 35000 < tax <= 55000:
+        tax_amount = tax * 0.3 - 2755
+    elif 55000 < tax <= 80000:
+        tax_amount = tax * 0.35 - 5505
+    else:
+        tax_amount = tax * 0.45 - 13505
+    return tax_amount
+
+
+if __name__=='__main__':
+   if len(sys.arvg) != 2:
+        print('Parameter Error')
+   for avg in sys.arvg[1:]:
+        income = avg.split[':'][1]
+        taxable_income = tax(income)
+        taxable_amount = amount(taxable_income)
+        money = income - taxable_income - taxable_amount
+        
