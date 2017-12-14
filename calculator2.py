354fac92116b75ee917579488ad44482c51c2c3b
kimcho <330596977@qq.com>
2017-12-14 20:16:18 +0800

    Update calculator2.py
    version 2

--- calculator2.py
+++ calculator2.py
@@ -1,43 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
-if len(sys.argv) != 2 or len(sys.argv) == 1:
-    print("Parameter Error")
-    exit()
-try:
-    income = int(sys.argv[1])
-except ValueError:
-    print("Parameter Error")
-    exit()
-finally:
-    if income - 3500 <= 0:
-        amount = 0.00
-    elif income - 3500 <= 1500:
-        amount = (income - 3500) * 0.03 - 0.00
-    elif income - 3500 <= 4500:
-        amount = (income - 3500) * 0.01 - 0.00
-    elif income - 3500 <= 9000:
-        amount = (income - 3500) * 0.02 - 0.00
-    elif income - 3500 <= 35000:
-        amount = (income - 3500) * 0.25 -1055
-    elif income - 3500 <= 55000:
-        amount = (income - 3500) * 0.3 - 2755
-    elif income - 3500 <= 80000:
-        amount = (income - 3500) * 0.35 - 5505
-    else:
-        amount = (income - 3500) * 0.45 - 13505
-    print(format(amount, ".2f"))
+
+def socialSecurity(income):
+    social_security = income * 0.165
+    return social_security
 
 
-
-
-
-def tax(income):
-    income = int(avg.split(':')[1])
-    taxable = income - income * 0.165 - 3500
-    return taxable
-
-def amount(tax):
+def amount(income):
+    tax = income - income * 0.165 - 3500
     if tax <= 0:
         tax_amount = 0
     elif tax <= 1500:
@@ -57,12 +28,19 @@ def amount(tax):
     return tax_amount
 
 
-if __name__=='__main__':
-   if len(sys.arvg) != 2:
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
         print('Parameter Error')
-   for avg in sys.arvg[1:]:
-        income = avg.split[':'][1]
-        taxable_income = tax(income)
-        taxable_amount = amount(taxable_income)
-        money = income - taxable_income - taxable_amount
-        
+        exit()
+    for avg in sys.argv[1:]:
+        try:
+            month_income = float(avg.split(':')[1])
+        except ValueError:
+            print('Parameter Error')
+            exit()
+        social_Security = social(month_income)
+        print(social_Security)
+        taxable_amount = amount(month_income)
+        print(taxable_amount)
+        money = month_income - social_Security - taxable_amount
+        print(avg.split(':')[0] + ":" + format(money, '.2f'))
