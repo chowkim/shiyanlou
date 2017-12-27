7f60be3b5a1dd2f705a794a5940f18d3c1708676
Wong <330596977@qq.com>
2017-12-27 23:27:50 +0800

    Refactoring code from collections import namedtuple

--- week3/shiyanlou_github_spider.py
+++ week3/shiyanlou_github_spider.py
@@ -12,7 +12,7 @@ class ShiYanLouCodeSpider(scrapy.Spider):
         return(start_urls.format(i) for i in range(1, 5))
 
     def parse(self, response):
-        for url in response.css('li.col-12.d-block.width-full.py-4.border-bottom.public.source'):
+        for url in response.css('li.col-12'):
             yield {
                     'name': url.css('div.d-inline-block.mb-1 a::text').re_first('[^\s]+[\w]*'),
                     'update_time': url.xpath('//*[@id="user-repositories-list"]/ul/li/div/relative-time/@datetime').extract_first()
