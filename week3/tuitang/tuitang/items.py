90961c2d3b407dbd050953e321b16356c0769ed9
kimcho <330596977@qq.com>
2018-01-02 19:08:25 +0800

    Update items.py

--- week3/tuitang/tuitang/items.py
+++ week3/tuitang/tuitang/items.py
@@ -11,5 +11,5 @@ import scrapy
 class ImageItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    image_url = scrapy.Field()
+    image_urls = scrapy.Field()
     images = scrapy.Field()
