2bf63d0a5c39f1326c25aa48b2eb2ae1966a89a8
kimcho <330596977@qq.com>
2018-01-11 16:11:23 +0800

    User.query.filter_by(username=username).first()
    User.query.filter_by(username=username).first()

--- week5/simpledu/simpledu/handlers/user.py
+++ week5/simpledu/simpledu/handlers/user.py
@@ -1,15 +1,11 @@
 from flask import Blueprint
 from flask import render_template, abort
-from simpledu.models import Course, User
+from simpledu.models import User
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
 
 @user.route('/<username>')
 def user_index(username):
-    #user_item = Course.query.all()
-    user_item = User.equey.filt.
-    user_item = User.equey.filt.
-    return render_template('user.html', user_item=user_item)
-
-
+    user = User.query.filter_by(username=username).first()
+    return render_template('user.html', user=user)
