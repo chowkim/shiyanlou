64f4348856d631839f28b29ac591d570f95357f9
kimcho <hins110@163.com>
2018-01-11 12:00:31 +0800

    save

---
+++ week5/simpledu/simpledu/handlers/user.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask import render_template, abort
+from simpledu.models import Course, User
+
+user = Blueprint('user', __name__, url_prefix='/user')
+
+
+@user.route('/<username>')
+def user(username):
+    #user_item = Course.query.all()
+    user_item = User.equey.filt.
+    user_item = User.equey.filt.
+    return render_template('user.html', user_item=user_item)
+
+
